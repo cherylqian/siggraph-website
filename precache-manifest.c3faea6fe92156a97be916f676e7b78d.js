47fc04585e4a4df9d728b28e700bcbf49eb3665b
Chen Qian <chenq2@illinois.edu>
2018-10-01 22:40:22 -0500

    Updates

---
+++ precache-manifest.c3faea6fe92156a97be916f676e7b78d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/siggraph-website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5ecdf5ade8c882803350",
+    "url": "/siggraph-website/static/css/main.7c76159e.chunk.css"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/siggraph-website/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d3b8ffc80c9ac28d3f22",
+    "url": "/siggraph-website/static/js/1.d3b8ffc8.chunk.js"
+  },
+  {
+    "revision": "537fea2527fe2249e9ac",
+    "url": "/siggraph-website/static/js/runtime~main.537fea25.js"
+  },
+  {
+    "revision": "5ecdf5ade8c882803350",
+    "url": "/siggraph-website/static/js/main.5ecdf5ad.chunk.js"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/siggraph-website/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/siggraph-website/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/siggraph-website/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d3b8ffc80c9ac28d3f22",
+    "url": "/siggraph-website/static/css/1.c26a8e32.chunk.css"
+  },
+  {
+    "revision": "1ca309faaa465948318d1b2f7da39601",
+    "url": "/siggraph-website/index.html"
+  }
+];
